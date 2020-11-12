7f414b30fa1c3814b687434b0f05a25a5a43aaad
Paul Gregoire <mondain@gmail.com>
2020-11-12 07:09:45 -0800

    Fix for possible race condition binding UDP socket

--- src/main/java/org/ice4j/socket/IceUdpSocketWrapper.java
+++ src/main/java/org/ice4j/socket/IceUdpSocketWrapper.java
@@ -77,19 +77,20 @@ public class IceUdpSocketWrapper extends IceSocketWrapper {
                                 logger.debug("No session, searching transport for: {} to: {}", transportAddress, destAddress);
                                 sess = transport.getSessionByLocal(transportAddress);
                             } else {
+                                logger.debug("Port is not bound: {}", portBound);
                                 // bind it
-                                transport.addBinding(transportAddress);
+                                //transport.addBinding(transportAddress);
                             }
                             // if the session wasn't found elsewhere, create one
                             if (sess == null) {
                                 // verify that the address can be reached first
-                                if (InetAddress.getByName(((InetSocketAddress) destAddress).getHostString()).isReachable(500)) {
+                                //if (InetAddress.getByName(((InetSocketAddress) destAddress).getHostString()).isReachable(500)) {
                                     logger.debug("No session, attempting connect from: {} to: {}", transportAddress, destAddress);
                                     // attempt to create a server session, if it fails the local address isn't bound
                                     sess = transport.createSession(this, destAddress);
-                                } else {
-                                    logger.warn("Destination address: {} not reachable from: {}", destAddress, transportAddress);
-                                }
+                                //} else {
+                                //    logger.warn("Destination address: {} not reachable from: {}", destAddress, transportAddress);
+                                //}
                             }
                         } catch (Exception e) {
                             logger.warn("Exception getting session for: {}", transportAddress, e);
